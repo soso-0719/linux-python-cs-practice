dc1976d535a23e6ff1da1cf6a3ee746bb8b39f79
sou11 <sou11215@icloud.com>
2026-05-28 17:17:02 +0900

    improve calc.py

--- calc.py
+++ calc.py
@@ -1,22 +1,47 @@
 print("Simple Calculator")
 print("Available operators: +, -, *, /")
+print("Type q to quit")
 
-a = int(input("first number: "))
-operator = input("operator: ")
-b = int(input("second number: "))
+while True:
+    first_input = input("first number: ")
 
-if operator == "+":
-    result = a + b
-elif operator == "-":
-    result = a - b
-elif operator == "*":
-    result = a * b
-elif operator == "/":
-    if b == 0:
-        result = "cannot divide by zero"
-    else:
-        result = a / b
-else:
-    result = "unknown operator"
+    if first_input == "q":
+        print("bye")
+        break
 
-print("result:", result)
+    operator = input("operator: ")
+
+    if operator == "q":
+        print("bye")
+        break
+
+    second_input = input("second number: ")
+
+    if second_input == "q":
+        print("bye")
+        break
+
+    try:
+        a = int(first_input)
+        b = int(second_input)
+
+        if operator == "+":
+            result = a + b
+        elif operator == "-":
+            result = a - b
+        elif operator == "*":
+            result = a * b
+        elif operator == "/":
+            if b == 0:
+                result = "cannot divide by zero"
+            else:
+                result = a / b
+        else:
+            result = "unknown operator"
+
+        print("result:", result)
+
+    except ValueError:
+        print("please enter numbers only")
+
+    print()
