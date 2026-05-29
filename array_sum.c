33620debe4abbcfdfb596f46e44ea650e79c019c
sou11 <sou11215@icloud.com>
2026-05-29 10:49:42 +0900

    compare compiler improve array_sum.c

--- array_sum.c
+++ array_sum.c
@@ -1,17 +1,23 @@
 #include <stdio.h>
-
+#include <time.h>
 int main(void) {
-    int numbers[5] = {1, 2, 3, 4, 5};
-    int total = 0;
-
-    for (int i = 0; i < 5; i++) {
-        total += numbers[i];
-
-        printf("numbers[%d] value: %d, address: %p\n",
-               i, numbers[i], (void *)&numbers[i]);
+    int N ;
+    printf("C言語における実行時間の検証");
+    printf("Nの値を入力してください");
+    scanf("%d", &N);
+    long long  total = 0;
+    clock_t start = clock();
+    
+    for (int i = 0; i < N; i++) {
+        total += i;
     }
 
-    printf("total: %d\n", total);
+    clock_t end = clock();
 
+     double elapsed = (double)(end - start) / CLOCKS_PER_SEC;
+    
+    printf("N: %d\n", N);
+    printf("total: %lld\n", total);
+    printf("elapsed: %f seconds\n", elapsed);
     return 0;
 }
